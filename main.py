0d93ae081fbabd5b394e5ad5271cc3db10684a64
Alexandr <37587635+AlexandrIoda@users.noreply.github.com>
2018-04-04 19:27:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 import smtplib
 
-sender = "dhcptester@gmail.com"  # Email of sender
-psw = "beGITeglypcy"  # password
-recipient = ["budkomaximidu55555@gmail.com", "sashaioda12345@gmail.com"]  # Email of recipients
-message = "Hello my dear friend!"  # message
+sender = "dhcptester@gmail.com"  #Email of sender
+psw = "beGITeglypcy"  #password
+recipient = ["budkomaximidu55555@gmail.com", "sashaioda12345@gmail.com"]  #Email of recipients
+message = "Hello my dear friend!"  #message
 
-
-def sendToEmail(sender, psw, recipient, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-
-    try:
-        server.starttls()
-        server.login(sender, psw)
-        server.sendmail(sender, recipient, message)
-
-        return True
-    except Exception as e:
-        print("The message was not sent")
-        print(e)
-    finally:
-        server.quit()
-
-    return False
-
-
-sendToEmail(sender, psw, recipient, message)
+server = smtplib.SMTP("smtp.gmail.com", 587) #connecting with email server
+server.starttls() #encryption
+server.login(sender, psw)
+server.sendmail(sender, recipient, message) #send to email
+server.quit()
